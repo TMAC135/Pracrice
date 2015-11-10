5a52c4070363f4a134eefe712d5f396c91fcf050
TianRan <TianRan@TianRantekiMacBook-Air.local>
2015-11-10 00:23:09 -0600

    update the nonrecursive way

--- subset.py
+++ subset.py
@@ -38,7 +38,7 @@ class Solution:
 	# we also need a list tmp to add elements base on the previous information.
 	"""
 	show byexample:
-				     [ ]      	  tmp,s=[],[1 2 3]
+					 [ ]      	  tmp,s=[],[1 2 3]
 				   /  \  \
 				[1]  [2]  [3]     tmp,s=[],[1 2 3] ; [],[2 3]  ; [],[3]
 			   /  \     \
@@ -54,22 +54,72 @@ class Solution:
 
 
 	def dfs(self,s,tmp,res):
+		# call the list to be added valuelist
 		if s:
 			for i in xrange(len(s)):
 				res.append(tmp+[s[i]])
 				self.dfs(s[(i+1):],tmp+[s[i]],res)
 
-	# Method of Interation
-	def subsets2(self,S):
+	# Method of Bit manipulation
 
+	"""
+	Copy from others solution, use bit manipulation:
+	相当牛逼的bit解法。基本的想法是，用bit位来表示这一位的number要不要取，第一位有1，0即取和不取2种可能性。所以只要把0到N种可能
+	都用bit位表示，再把它转化为数字集合，就可以了。
+	http://www.fusu.us/2013/07/the-subsets-problem.html
+
+	"""
+	def subsets2(self, S):
+		S.sort()
+		k = len(S)
+		n = 2 ** k
+		res = []
+		for i in range(n):
+			s = self.filter(S, k, i)
+			res.append(s)
+		return res
+
+	"""
+	这个filter 函数可以拓展为任意的 bit select 问题，给定一个list,长度是k,我们输入任意
+	数字 0 <= i <= 2^k - 1, 可以表示为长度为k的二进制表达式， 若该位为0，则不选择，
+	若为1则选择。
+	"""
+	def filter(self, S, k, i):
+		res = []
+		for j in range(k):
+			mask = 1 << j
+			if i & mask > 0:
+				res.append(S[j])
+		return res
+
+	# Method of Interation
+	"""
+	First, consider there is no duplicates, how to generate the subsets?
+	Say n is the # of the elements,
+	when n=1, subsets :  {}, {"1"},  "i" means the ith element.
+	when n=2, subsets:   {}, {"1"}, {"2"}, {"1", "2"}
+	when n=3, subsets:   {}, {"1"}, {"2"}, {"1", "2"}, {"3"}, {"1","3"}, {"2","3"}, {"1", "2","3"}
+	So, the way of generating subsets is:
+	for current element, add this element to all the subsets list in the res
+
+	"""
+
+	def subsets3(self,S):
+		if not S:
+			return S
+		S.sort()
+		res=[[]]
+		for i in S:
+			add=[x+[i] for x in res]
+			res+=add
+		return res
 
 
 
 
 
 if __name__=='__main__':
-	print Solution().subsets1([1,3,2]);
-
+	print Solution().subsets3([1,3,2]);
 
 
 
--- subsetsII.py
+++ subsetsII.py
@@ -32,6 +32,7 @@ class Solution:
 	this method base on the subset.py, we only consider the case where 
 	"""
 
+	# Method of recursion
 	def subsetsWithDup(self, S):
 		# write your code here
 		S.sort()
@@ -50,8 +51,42 @@ class Solution:
 					prev=s[i]
 
 
+	# Method of Interation
+	"""
+	Then we take the duplicates into account, the same example:
+	when n=1, subsets :  {}, {"1"},  "i" means the ith element.
+	when n=2, subsets:   {}, {"1"}, {"2"}, {"1", "2"}
+	when n=3, but "2"=="3" subsets: 
+	   {}, {"1"}, {"2"}, {"1", "2"}, {"3"}, {"1","3"}, {"2","3"}, {"1", "2","3"}
+	since "2"=="3", which truly is:
+	   {}, {"1"}, {"2"}, {"1", "2"}, {"2"}, {"1","2"}, {"2","2"}, {"1", "2","2"}
+	where the bold ones are not needed.
+	So, how these two subsets are generated? They are from the subsets of n=1.
+
+	In sum up, when meet the same element as previous one, then generate new subsets ONLY 
+	from the subsets generated from previous iteration, other than the whole subsets list.
+	"""
+
+	def subsetsWithDup2(self,S):
+		if not S:
+			return S
+		S.sort()
+		res=[[]]
+		# prev_add is used to deal with the case of current element is same as previous one
+		prev_add=[]
+		prev=None
+		for i in xrange(len(S)):
+			if S[i] != prev:
+				add = [ x + [S[i]] for x in res]
+			else:
+				add = [ x + [S[i]] for x in prev_add] # if the same, we can only add the element from previous add
+			res += add
+			prev_add=add
+			prev=S[i]
+		return res
+
 if __name__=='__main__':
-	print Solution().subsetsWithDup([1,2,2])
+	print Solution().subsetsWithDup2([1,1,2,2])
 
 
 
